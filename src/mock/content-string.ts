0fec3e256276c0d049d945bc6722b84c7624d91d
gildembergleite <gildembergleite@gmail.com>
2024-05-11 09:24:32 -0300

    feat: created mock data for this map

---
+++ src/mock/content-string.ts
@@ -0,0 +1,13 @@
+export const contentString =
+  '<div id="content" style="color: black;">' +
+  '<div id="siteNotice">' +
+  '</div>' +
+  '<h1 id="firstHeading" class="firstHeading" style="color: black;">Apresentação: Como utilizar o Google Maps em aplicações React</h1>' +
+  '<div id="bodyContent" style="color: black;">' +
+  '<p>Nesta apresentação, vamos explorar como integrar o Google Maps em aplicações React. Vamos aprender como mostrar mapas, adicionar marcadores, e personalizar a experiência do usuário.</p>' +
+  '<p>Discutiremos as diferentes APIs do Google Maps disponíveis para desenvolvedores React e como começar a usá-las em seus projetos.</p>' +
+  '<p>Você também aprenderá dicas e truques para otimizar o desempenho e a usabilidade do Google Maps em sua aplicação React.</p>' +
+  '<p>Esta apresentação é ideal para desenvolvedores React que desejam adicionar recursos de mapas às suas aplicações.</p>' +
+  '<p>Venha participar e descubra como levar suas aplicações React para o próximo nível com o Google Maps!</p>' +
+  '</div>' +
+  '</div>'
