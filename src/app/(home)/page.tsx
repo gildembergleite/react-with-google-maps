307fa967bb08229f445a122926440efa26b3e0f0
gildembergleite <gildembergleite@gmail.com>
2024-05-11 09:27:25 -0300

    feat: insert components in HomePage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,11 @@
+import { AsideBar } from '@/components/aside-bar'
+import { Map } from '@/components/map'
+
 export default function HomePage() {
   return (
-    <main className="flex w-screen h-screen items-center justify-center bg-zinc-900">
-      <h1 className="text-white font-bold text-4xl">Hello, World!</h1>
+    <main className="flex w-screen h-screen items-center justify-center">
+      <AsideBar />
+      <Map />
     </main>
   )
 }
