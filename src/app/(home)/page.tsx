13c04addad510e7747f3b4937b2ba6334f2b8954
gildembergleite <gildembergleite@gmail.com>
2024-09-09 23:19:29 -0300

    refactor: disabled cache

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,7 +5,9 @@ export default async function HomePage() {
   const url =
     process.env.NEXT_PUBLIC_API_BASE_URL +
     '/rastreamento/coordinates?page_size=200'
-  const response = await fetch(url)
+  const response = await fetch(url, {
+    cache: 'no-cache',
+  })
   const { data: coordinates } = await response.json()
 
   return (
